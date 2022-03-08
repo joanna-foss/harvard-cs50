2ccc54fa8fa6ce95534f4be997fe35dfbb3955b0
Joanna <m.joanna.foss@gmail.com>
2022-03-08 17:48:11 -0600

    hello world in c

--- hello.c
+++ hello.c
@@ -1,6 +1,10 @@
-#include<stdio.h>
+#include <cs50.h>
+#include <stdio.h>
 
 int main(void)
 {
-    printf("hello, world\n");
+    string answer = get_string("What's your name?");
+    printf("hello, %s\n", answer);
 }
+
+//command line: clang hello.c -o hello -lcs50  <---what is happening here?
