1a05dd94af201e432473fc0cc46a318af8f74649
Joanna <m.joanna.foss@gmail.com>
2022-03-08 22:36:20 -0600

    some notes on calculations in c

--- calculator.c
+++ calculator.c
@@ -3,8 +3,19 @@
 
 int main(void)
 {
-    int x = get_int("x: ");
-    int y = get_int("y: ");
-    int total = x + y;
-    printf("total: %i\n", total);
+    //Prompt user for x
+    float x = get_float("x: ");
+
+    //Prompt user for y
+    float y = get_float("y: ");
+
+    //Do maths
+    float total = x / y;
+
+    //Print solution
+    printf("total: %f\n", total);
 }
+
+//floating-point precision: fundamental problem where are maths are not accurate 100% of the time.
+//truncation: int / int throws away anything beyond decimal if result saved as float
+//19 January 2038 world ends??? --total number of seconds! 2 billion is the max. More bits!
