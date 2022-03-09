528ef7a9989618f5ae4c165de5e94969a24e696f
Joanna <m.joanna.foss@gmail.com>
2022-03-08 18:52:07 -0600

    minor edit

--- hello.c
+++ hello.c
@@ -1,5 +1,5 @@
-#include <cs50.h>
-#include <stdio.h>
+#include <cs50.h> //header file
+#include <stdio.h> //header file
 
 int main(void)
 {
@@ -7,4 +7,6 @@ int main(void)
     printf("hello, %s\n", answer);
 }
 
-//command line: clang hello.c -o hello -lcs50  <---what is happening here?
+//command line: clang hello.c -o hello -lcs50  <---what is happening here? compiles?
+//then run code using ./hello
+//in c there is no official string data type which is why we must implement the <cs50.h> to  be able to 
