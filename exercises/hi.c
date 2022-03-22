6b3d4dc458b3ea8ac842be1ca50f44ffe105e6ac
Joanna <m.joanna.foss@gmail.com>
2022-03-21 23:26:00 -0500

    line 11 spacing

--- exercises/hi.c
+++ exercises/hi.c
@@ -8,7 +8,7 @@ int main(void)
     char c3 = '!';
 
     printf("%c%c%c\n", c1, c2, c3); //output: HI!
-    printf("%i%i%i\n", c1, c2, c3); //implicit type casting output: 727333
+    printf("%i %i %i\n", c1, c2, c3); //implicit type casting output: 72 73 33
     printf("%i %i %i\n", (int) c1, (int) c2, (int) c3); //explicit type casting output: 72 73 33
 
     string s = "HI!";
