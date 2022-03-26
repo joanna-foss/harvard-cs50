5cf9269742df7aa3a5ca48c2d0911900f23d63e5
Joanna <m.joanna.foss@gmail.com>
2022-03-25 21:44:16 -0500

    structuring

--- exercises/functions.c
+++ exercises/functions.c
@@ -1,13 +1,24 @@
 #include <cs50.h>
 #include <stdio.h>
 
+//declaring functions
+float multiply_two_floats(float a, float b);
+int add_two_ints(int a, int b);
+
+int main(void)
+{
+    float two_floats_multiplied = multiply_two_floats(3.4, 3.0);
+    int sum_of_two_ints = add_two_ints(33, 22);
+    printf("The result is %i\n", sum_of_two_ints);
+}
+
+
 float multiply_two_floats(float a, float b)
 {
     return a * b;
 }
 
-int main(void)
+int add_two_ints(int a, int b)
 {
-    float number = multiply_two_floats(3.4, 3.0);
-    printf("The result is %f\n", number);
+    return a + b;
 }
